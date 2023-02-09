03555b42d888d7de3a03d9852a13ff17490b6b4c
DeanStavenuiter <info@deanstavenuiter.nl>
2023-02-09 17:20:22 +0100

    added views, routes

--- ecommerce/models/User.model.js
+++ ecommerce/models/User.model.js
@@ -1,5 +1,10 @@
 const { Schema, model } = require("mongoose");
 
+const validateEmail = function (email) {
+  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+  return re.test(email)
+}
+
 const userSchema = new Schema(
   {
     firstName: {
@@ -18,6 +23,7 @@ const userSchema = new Schema(
       unique: true,
       lowercase: true,
       trim: true,
+      validate: [validateEmail, 'Please fill a valid email address'],
     },
     passwordHash: {
       type: String,
--- ecommerce/routes/auth.routes.js
+++ ecommerce/routes/auth.routes.js
@@ -1,19 +1,48 @@
 const express = require("express");
 const router = express.Router();
+const UserModel = require('../models/User.model');
+const bcrypt = require('bcryptjs')
 
 //signup get route
 router.get("/signup", (req, res) => {
-  res.render("/auth/signup");
+  res.render("auth/signup");
 });
 
 //post route to sent the signup information
-router.post("/signup", (req, res) => {
-  res.redirect("/profile");
-});
+router.post('/signup', async (req, res) => {
+    const body = { ...req.body }
+  
+    // if (body.password.length < 6) {
+    //   res.render('auth/signup', { errorMessage: 'Password too short', body: req.body })
+    // } else {
+      const salt = bcrypt.genSaltSync(13)
+      const passwordHash = bcrypt.hashSync(body.password, salt)
+  
+      delete body.password
+      body.passwordHash = passwordHash
+    console.log(body)
+      try {
+        await UserModel.create(body)
+        res.send(body)
+      } catch (error) {
+        if (error.code === 11000) {
+          res.render('auth/signup', {
+            errorMessage: 'Username already used !',
+            userData: req.body,
+          })
+        } else {
+          res.render('auth/signup', {
+            errorMessage: error,
+            userData: req.body,
+          })
+        }
+      }
+    // }
+  })
 
 //login get route
 router.get("/login", (req, res, ) => {
-  res.render("/auth/login");
+  res.render("auth/login");
 });
 
 //post route to sent the login information
@@ -30,6 +59,6 @@ router.post('/logout', (req, res) => {
   });
 
 
-  
+
 module.exports = router;
 
