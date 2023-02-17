eaa39362c7d01a966f9f7769085a6286d9c996ff
Fabrizio Giffi <fabrizio.giffi@gmail.com>
2023-02-17 11:53:44 +0100

    hopefully last commit before presentation :D

--- ecommerce/routes/auth.routes.js
+++ ecommerce/routes/auth.routes.js
@@ -4,10 +4,9 @@ const UserModel = require("../models/User.model");
 const bcrypt = require("bcryptjs");
 const { isLoggedOut, isLoggedIn } = require("../middleware/route-guard");
 
-
 //signup get route
 router.get("/signup", isLoggedOut, (req, res) => {
-  res.render("auth/register", {user: req.session.user});
+  res.render("auth/register", { user: req.session.user });
 });
 
 //post route to sent the signup information
@@ -15,48 +14,55 @@ router.post("/signup", isLoggedOut, async (req, res) => {
   const body = { ...req.body };
 
   if (body.password.length < 6) {
-    res.render('auth/register', { errorMessage: 'Password too short', body: req.body })
+    res.render("auth/register", {
+      error: "password",
+      userData: req.body,
+    });
   } else {
-  const salt = bcrypt.genSaltSync(13);
-  const passwordHash = bcrypt.hashSync(body.password, salt);
+    const salt = bcrypt.genSaltSync(13);
+    const passwordHash = bcrypt.hashSync(body.password, salt);
 
-  delete body.password;
-  body.passwordHash = passwordHash;
-  try {
-    const user = await UserModel.create(body);
+    delete body.password;
+    body.passwordHash = passwordHash;
+    try {
+      const user = await UserModel.create(body);
 
-    const tempUser = {
-      id: user._id,
-      firstName: user.firstName,
-      lastName: user.lastName,
-      email: user.email,
-    };
+      const tempUser = {
+        id: user._id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+      };
 
-   
+      req.session.user = tempUser;
+      req.session.cart = user.cart;
 
-    req.session.user = tempUser;
-    req.session.cart = user.cart;
-
-    res.redirect(`/profile/${user.firstName.split(" ").join("")}-${user.lastName.split(" ").join("")}`);
-  } catch (error) {
-    if (error.code === 11000) {
-      res.render("auth/register", {
-        errorMessage: "Username already used !",
-        userData: req.body,
-      }, {user: req.session.user});
-    } else {
-      res.render("auth/register", {
-        errorMessage: error,
-        userData: req.body,
-      }, {user: req.session.user});
+      res.redirect(
+        `/profile/${user.firstName.split(" ").join("")}-${user.lastName
+          .split(" ")
+          .join("")}`
+      );
+    } catch (error) {
+      if (error.code === 11000) {
+        res.render("auth/register", {
+          error: "email",
+          userData: req.body,
+          user: req.session.user,
+        });
+      } else {
+        res.render("auth/register", {
+          errorMessage: error,
+          userData: req.body,
+          user: req.session.user,
+        });
+      }
     }
-  }
   }
 });
 
 //login get route
 router.get("/login", isLoggedOut, (req, res) => {
-  res.render("auth/login", {user: req.session.user});
+  res.render("auth/login", { user: req.session.user });
 });
 
 //post route to sent the login information
@@ -80,13 +86,17 @@ router.post("/login", isLoggedOut, async (req, res) => {
       // If password is correct, create a session for the user
       req.session.user = tempUser;
       req.session.cart = user.cart;
-      
-      res.redirect(`/profile/${user.firstName.split(" ").join("")}-${user.lastName.split(" ").join("")}`);
+
+      res.redirect(
+        `/profile/${user.firstName.split(" ").join("")}-${user.lastName
+          .split(" ")
+          .join("")}`
+      );
     } else {
-      res.render("auth/login", {error: "Password", user: req.session.user})
+      res.render("auth/login", { error: "Password", user: req.session.user });
     }
   } else {
-    res.render("auth/login", {error: "Email", user: req.session.user})
+    res.render("auth/login", { error: "Email", user: req.session.user });
   }
 });
 
